a0be4c5feb03c83949635ddba9eeaed0a6397625
Kelly Bennett <cornerkick87@gmail.com>
2018-03-12 15:07:40 -0700

    added more text, javascript update and css

---
+++ js/app.js
@@ -0,0 +1,35 @@
+
+
+var shopOne = {
+        location: "1st and Pike", 
+        minCust: 23,
+        maxCust:  65,
+        avgCookieSales: 3
+    }
+console.log(shopOne);
+var shopTwo = {
+        location: "Seatac Airport",
+        minCust: 3,
+        maxCust: 24,
+        avgCookieSales: 1.2 
+    }
+
+var shopThree = {
+        location: "Seattle Center",
+        minCust: 11,
+        maxCust: 38,
+        avgCookieSales: 3.7
+    }
+var shopThree = {
+        location: "Capitol Hill"
+        minCust: 20,
+        maxCust: 38,
+        avgCookieSales: 2.3
+    }
+
+var ShopFour = {
+        location: "Akli"
+        minCust: 2, 
+        maxCust: 16,
+        avgCookieSales: 46
+    }
