fb1b4474cc1e03091c2ca99fa5fee4c52441b977
Kelly Bennett <cornerkick87@gmail.com>
2018-03-12 19:55:40 -0700

    updated css html

--- js/app.js
+++ js/app.js
@@ -1,35 +1,108 @@
 
 
-var shopOne = {
-        location: "1st and Pike", 
-        minCust: 23,
-        maxCust:  65,
-        avgCookieSales: 3
-    }
-console.log(shopOne);
-var shopTwo = {
-        location: "Seatac Airport",
-        minCust: 3,
-        maxCust: 24,
-        avgCookieSales: 1.2 
-    }
+var shops =  [ {
+    location: "1st and Pike", 
+    minCust: 23,
+    maxCust:  65,
+    avgCookieSales: 3,
+    
+    showRandomCustCount: function() {
+        this.cust =[];
+        this.sales = [];
+        for(var i = 0; i < 15; i++) {
+            var hrlyCust = Math.floor (Math.random()* (this.maxCust-this.minCust)) + 1;
 
-var shopThree = {
-        location: "Seattle Center",
-        minCust: 11,
-        maxCust: 38,
-        avgCookieSales: 3.7
+            var hrlySales = hrlyCust * this.avgCookieSales;
+         
+            this.sales.push(hrlySales);
+            console.log('Sales' + this.sales);
+            this.cust.push(hrlyCust);
+            console.log('customers' + this.cust);
+        }
+            
     }
-var shopThree = {
-        location: "Capitol Hill"
-        minCust: 20,
-        maxCust: 38,
-        avgCookieSales: 2.3
-    }
+},
 
-var ShopFour = {
-        location: "Akli"
-        minCust: 2, 
-        maxCust: 16,
-        avgCookieSales: 46
+{
+    location: "Seatac Airport",
+    minCust: 3,
+    maxCust: 24,
+    avgCookieSales: 1.2,
+    showRandomCustCount: function() {
+        this.cust =[];
+        this.sales = [];
+        for(var i = 0; i < 15; i++) {
+            var hrlyCust = Math.floor (Math.random()* (this.maxCust-this.minCust)) + 1;
+
+            var hrlySales = hrlyCust * this.avgCookieSales;
+         
+            this.sales.push(hrlySales);
+            console.log('Sales' + this.sales);
+            this.cust.push(hrlyCust);
+            console.log('customers' + this.cust);
+        }
+    } 
+},
+
+{
+    location: "Seattle Center",
+    minCust: 11,
+    maxCust: 38,
+    avgCookieSales: 3.7,
+    showRandomCustCount: function() {
+        this.cust =[];
+        this.sales = [];
+        for(var i = 0; i < 15; i++) {
+            var hrlyCust = Math.floor (Math.random()* (this.maxCust-this.minCust)) + 1;
+
+            var hrlySales = hrlyCust * this.avgCookieSales;
+         
+            this.sales.push(hrlySales);
+            console.log('Sales' + this.sales);
+            this.cust.push(hrlyCust);
+            console.log('customers' + this.cust);
+        }
+    }   
+},
+
+{
+    location: "Capitol Hill",
+    minCust: 20,
+    maxCust: 38,
+    avgCookieSales: 2.3,
+    showRandomCustCount: function() {
+        this.cust =[];
+        this.sales = [];
+        for(var i = 0; i < 15; i++) {
+            var hrlyCust = Math.floor (Math.random()* (this.maxCust-this.minCust)) + 1;
+
+            var hrlySales = hrlyCust * this.avgCookieSales;
+         
+            this.sales.push(hrlySales);
+            console.log('Sales' + this.sales);
+            this.cust.push(hrlyCust);
+            console.log('customers' + this.cust);
+        }
     }
+},
+{
+    location: "Akli",
+    minCust: 2, 
+    maxCust: 16,
+    avgCookieSales: 46,
+    showRandomCustCount: function() {
+        this.cust =[];
+        this.sales = [];
+        for(var i = 0; i < 15; i++) {
+            var hrlyCust = Math.floor (Math.random()* (this.maxCust-this.minCust)) + 1;
+
+            var hrlySales = hrlyCust * this.avgCookieSales;
+         
+            this.sales.push(hrlySales);
+            console.log('Sales' + this.sales);
+            this.cust.push(hrlyCust);
+            console.log('customers' + this.cust);
+        }
+    }
+}
+];
