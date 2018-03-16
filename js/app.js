b3dea10c8af8d14c7b4916f822e077798795ac9f
Kelly Bennett <cornerkick87@gmail.com>
2018-03-16 08:46:19 -0700

    fixed bug in js that was making my random numbers return NaN

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ Shop.prototype.calcCustCount = function(){
     for(var i = 0; i < 15; i++) {
         var hrlyCust = Math.floor(Math.random()* (this.maxCust-this.minCust) +this.minCust);
         
-        var hrlySales = hrlyCust * this.avgCookieSales;
+        var hrlySales = Math.floor(hrlyCust * this.avgCookieSales);
         
         this.sales.push(hrlySales);
         console.log('Sales' + this.sales);
@@ -41,201 +41,19 @@ Shop.prototype.showSales = function() {
 
 
 
-var shops =  [ {
-    location: "1st and Pike", 
-    minCust: 23,
-    maxCust:  65,
-    avgCookieSales: 3,
-    showSales: function() {
-        var tableRow = document.createElement('tr');
-        var salesTable = document.getElementById('salesTable');
-        salesTable.appendChild(tableRow);
-        
-        var tableData = document.createElement ('td');
-        tableRow.appendChild(tableData);
-        tableData.textContent = this.location;
-        
-        for(var i = 0; i < 15; i++) {
-            tableData = document.createElement ('td');
-            tableRow.appendChild(tableData);
-            tableData.textContent = this.sales[i];
-        }
-        
-    },
-    
-    calcCustCount: function() {
-        this.cust =[];
-        this.sales = [];
-        for(var i = 0; i < 15; i++) {
-            var hrlyCust = Math.floor(Math.random()* (this.maxCust-this.minCust)) + 1;
-            
-            var hrlySales = hrlyCust * this.avgCookieSales;
-            
-            this.sales.push(hrlySales);
-            console.log('Sales' + this.sales);
-            this.cust.push(hrlyCust);
-            console.log('customers' + this.cust);
-        }
-        
-    }
-},
-
-{
-    location: "Seatac Airport",
-    minCust: 3,
-    maxCust: 24,
-    avgCookieSales: 1.2,
-    showSales: function() {
-        var tableRow = document.createElement('tr');
-        var salesTable = document.getElementById('salesTable');
-        salesTable.appendChild(tableRow);
-        
-        var tableData = document.createElement ('td');
-        tableRow.appendChild(tableData);
-        tableData.textContent = this.location;
-        
-        for(var i = 0; i < 15; i++) {
-            tableData = document.createElement ('td');
-            tableRow.appendChild(tableData);
-            tableData.textContent = this.sales[i];
-        }
-        
-    },
-    
-    calcCustCount: function() {
-        this.cust =[];
-        this.sales = [];
-        for(var i = 0; i < 15; i++) {
-            var hrlyCust = Math.floor(Math.random() * (this.maxCust-this.minCust)) + 1;
-            
-            var hrlySales = hrlyCust * this.avgCookieSales;
-            
-            this.sales.push(hrlySales);
-            console.log('Sales' + this.sales);
-            this.cust.push(hrlyCust);
-            console.log('customers' + this.cust);
-        }
-    } 
-},
-
-{
-    location: "Seattle Center",
-    minCust: 11,
-    maxCust: 38,
-    avgCookieSales: 3.7,
-    showSales: function() {
-        var tableRow = document.createElement('tr');
-        var salesTable = document.getElementById('salesTable');
-        salesTable.appendChild(tableRow);
-        
-        var tableData = document.createElement ('td');
-        tableRow.appendChild(tableData);
-        tableData.textContent = this.location;
-        
-        for(var i = 0; i < 15; i++) {
-            tableData = document.createElement ('td');
-            tableRow.appendChild(tableData);
-            tableData.textContent = this.sales[i];
-        }
-        
-    },
-    
-    calcCustCount: function() {
-        this.cust =[];
-        this.sales = [];
-        for(var i = 0; i < 15; i++) {
-            var hrlyCust = Math.floor(Math.random()* (this.maxCust-this.minCust)) + 1;
-            
-            var hrlySales = hrlyCust * this.avgCookieSales;
-            
-            this.sales.push(hrlySales);
-            console.log('Sales' + this.sales);
-            this.cust.push(hrlyCust);
-            console.log('customers' + this.cust);
-        }
-    }   
-},
-
-{
-    location: "Capitol Hill",
-    minCust: 20,
-    maxCust: 38,
-    avgCookieSales: 2.3,
-    showSales: function() {
-        var tableRow = document.createElement('tr');
-        var salesTable = document.getElementById('salesTable');
-        salesTable.appendChild(tableRow);
-        
-        var tableData = document.createElement ('td');
-        tableRow.appendChild(tableData);
-        tableData.textContent = this.location;
-        
-        for(var i = 0; i < 15; i++) {
-            tableData = document.createElement ('td');
-            tableRow.appendChild(tableData);
-            tableData.textContent = this.sales[i];
-        }
-        
-    },
-    
-    calcCustCount: function() {
-        this.cust =[];
-        this.sales = [];
-        for(var i = 0; i < 15; i++) {
-            var hrlyCust = Math.floor(Math.random()* (this.maxCust-this.minCust)) + 1;
-            
-            var hrlySales = hrlyCust * this.avgCookieSales;
-            
-            this.sales.push(hrlySales);
-            console.log('Sales' + this.sales);
-            this.cust.push(hrlyCust);
-            console.log('customers' + this.cust);
-        }
-    }
-},
-{
-    location: "Alki",
-    minCust: 2, 
-    maxCust: 16,
-    avgCookieSales: 46,
-    showSales: function() {
-        var tableRow = document.createElement('tr');
-        var salesTable = document.getElementById('salesTable');
-        salesTable.appendChild(tableRow);
-        
-        var tableData = document.createElement ('td');
-        tableRow.appendChild(tableData);
-        tableData.textContent = this.location;
-        
-        for(var i = 0; i < 15; i++) {
-            tableData = document.createElement ('td');
-            tableRow.appendChild(tableData);
-            tableData.textContent = this.sales[i];
-        }
-        
-    },
-    
-    calcCustCount: function() {
-        this.cust =[];
-        this.sales = [];
-        for(var i = 0; i < 15; i++) {
-            var hrlyCust = Math.floor(Math.random()* (this.maxCust-this.minCust)) + 1;
-            
-            var hrlySales =Math.round (hrlyCust * this.avgCookieSales);
-            
-            this.sales.push(hrlySales);
-            console.log('Sales' + this.sales);
-            this.cust.push(hrlyCust);
-            console.log('customers' + this.cust);
-        }
-    }
-}
+var shops =  [
+    new Shop('1st and Pike', 23, 65, 3),
+    new Shop('Seatac Airport', 3, 24, 1.2),
+    new Shop('Seattle Center', 11, 38, 3.7),
+    new Shop('Capitol Hill', 20, 38, 2.3),
+    new Shop('Alki', 2, 16, 46)
 ];
 
-// function calcAndShowSales() {
-//     for(var i = 0; i < shops.length; i++) {
-//         shops[i].calcCustCount(); 
-//         shops[i].showSales(); 
-//     }
+function calcAndShowSales() {
+    for(var i = 0; i < shops.length; i++) {
+        shops[i].calcCustCount(); 
+        shops[i].showSales(); 
+    }
     
-// }
+}
+calcAndShowSales();
